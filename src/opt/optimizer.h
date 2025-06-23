3cebeb143835823b9e803d28c31fac3c3da8a435
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-23 13:26:49 +0300

    Update optimizer.h

--- src/opt/optimizer.h
+++ src/opt/optimizer.h
@@ -17,3 +17,16 @@
 void optimize_ir(IRInstruction *ir, int *count);
 
 #endif // OPTIMIZER_H
+
+#ifndef OPTIMIZER_H
+#define OPTIMIZER_H
+
+#include "../ir/ir_api.h"
+
+/**
+ * @brief Контроллер оптимизаций IR.
+ * Управляет последовательностью вызова различных оптимизаций.
+ */
+void optimizer_run(IR_Function *function);
+
+#endif // OPTIMIZER_H
