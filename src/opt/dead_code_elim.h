89ceec879e7636c19e2f42152b788ee8930b592c
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-23 13:28:00 +0300

    Update dead_code_elim.h

--- src/opt/dead_code_elim.h
+++ src/opt/dead_code_elim.h
@@ -47,3 +47,16 @@ int eliminate_dead_code(IRInstruction *ir, int count);
 int eliminate_dead_code(IRInstruction *ir, int count);
 
 #endif // DEAD_CODE_ELIM_H
+
+#ifndef DEAD_CODE_ELIM_H
+#define DEAD_CODE_ELIM_H
+
+#include "../ir/ir_api.h"
+
+/**
+ * @brief Удаляет неиспользуемые инструкции и ветвления в IR.
+ * Выполняет анализ живых переменных для определения мёртвого кода.
+ */
+void dead_code_elim(IR_Function *function);
+
+#endif // DEAD_CODE_ELIM_H
