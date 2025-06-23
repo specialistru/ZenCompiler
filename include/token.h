de123280f8fd2a4d5642c20be0160e425f3518d3
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-23 11:29:58 +0300

    Update token.h

---
+++ include/token.h
@@ -0,0 +1,30 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <stdint.h>
+
+// Типы токенов, встречающиеся при разборе ABAP
+typedef enum {
+    TOKEN_EOF,          // Конец файла
+    TOKEN_IDENTIFIER,   // Идентификатор (имена переменных, функций и т.п.)
+    TOKEN_KEYWORD,      // Ключевое слово языка
+    TOKEN_LITERAL,      // Литералы (числа, строки)
+    TOKEN_OPERATOR,     // Операторы (+, -, *, / и др.)
+    TOKEN_SEPARATOR,    // Разделители (, ; ( ) и др.)
+    TOKEN_COMMENT,      // Комментарии
+    TOKEN_UNKNOWN       // Неизвестный или ошибочный токен
+} TokenType;
+
+// Структура токена
+typedef struct {
+    TokenType type;      // Тип токена
+    char *text;          // Текст токена (строка)
+    uint32_t line;       // Номер строки в исходном коде
+    uint32_t column;     // Позиция в строке
+} Token;
+
+// Функции создания и уничтожения токена
+Token *token_create(TokenType type, const char *text, uint32_t line, uint32_t column);
+void token_destroy(Token *token);
+
+#endif // TOKEN_H
