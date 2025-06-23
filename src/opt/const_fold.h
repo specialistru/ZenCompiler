c5e66fbfb2fef87fa47f16e4be5700b12fe4bda5
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-23 12:53:13 +0300

    Update const_fold.h

--- src/opt/const_fold.h
+++ src/opt/const_fold.h
@@ -1,3 +1,4 @@
+/*
 #ifndef CONST_FOLD_H
 #define CONST_FOLD_H
 
@@ -10,6 +11,33 @@
 
 /**
  * @brief Выполняет замену выражений с константами на их значения.
+ *
+ * @param ir Массив IR-инструкций.
+ * @param count Количество инструкций.
+ */
+void const_fold(IRInstruction *ir, int count);
+
+#endif // CONST_FOLD_H
+*/
+
+#ifndef CONST_FOLD_H
+#define CONST_FOLD_H
+
+#include "ir.h"
+
+/**
+ * @file const_fold.h
+ * @brief Интерфейс модуля свёртывания констант IR.
+ */
+
+/**
+ * @brief Выполняет свёртывание выражений с константами и упрощение выражений.
+ *
+ * Реализует:
+ * - Арифметические операции: +, -, *, /, %
+ * - Логические операции: &&, ||, !
+ * - Сравнения: ==, !=, <, >, <=, >=
+ * - Peephole оптимизация (x = x + 0 → x)
  *
  * @param ir Массив IR-инструкций.
  * @param count Количество инструкций.
